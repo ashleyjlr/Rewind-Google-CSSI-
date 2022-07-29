669a3c35a089054a64dcaad72e043302fea0935b
XinYan Chen <>
2022-07-29 22:49:19 +0000

    lots of stuff

---
+++ music.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuLine = document.querySelectorAll(".menu-btn_find");
+let logoWord = document.querySelector("#logo_words");
+let sideBar = document.querySelector("#side_bar");
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    sideBar.style.display = "flex";
+    logoWord.style.color = "white";
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    sideBar.style.display = "none";
+    logoWord.style.color = "#335145";
+    menuOpen = false;
+  }
+});
