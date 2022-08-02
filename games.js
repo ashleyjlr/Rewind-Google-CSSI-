39969f0972115487680e7fef3df61860db53b718
XinYan Chen <>
2022-08-02 22:08:07 +0000

    fix

--- games.js
+++ games.js
@@ -32,12 +32,14 @@ g1Button.addEventListener("click", (e) => {
     g1Img.style.display = "none";
     console.log(button1click);
     document.getElementById("game1btn").innerHTML = "Close game!";
+    document.getElementById("game1game").innerHTML = ' <iframe width="560" height="315" allow="fullscreen; autoplay; encrypted-media" src="https://games.construct.net/1142/latest" frameborder="0" allowfullscreen="true" msallowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowpaymentrequest="false" referrerpolicy="unsafe-url" sandbox="allow-same-origin allow-forms allow-scripts allow-pointer-lock allow-orientation-lock allow-popups" scrolling="no"></iframe>';
   }
   else if (button1click%2 === 0){
     g1Img.removeAttribute('style');
     g1Game.style.display = "none";
     console.log(button1click);
     document.getElementById("game1btn").innerHTML = "Play now!";
+    document.getElementById("game1game").innerHTML = g1Img;
   }
 });
 
@@ -56,12 +58,14 @@ g2Button.addEventListener("click", (e) => {
     g2Img.style.display = "none";
     console.log(button2click);
     document.getElementById("game2btn").innerHTML = "Close game!";
+     document.getElementById("game2game").innerHTML = '<iframe width="560" height="315" allow="fullscreen; autoplay; encrypted-media" src="https://games.construct.net/274/latest" frameborder="0" allowfullscreen="true" msallowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowpaymentrequest="false" referrerpolicy="unsafe-url" sandbox="allow-same-origin allow-forms allow-scripts allow-pointer-lock allow-orientation-lock allow-popups" scrolling="no"></iframe>';
   }
   else if (button2click%2 === 0){
     g2Img.removeAttribute('style');
     g2Game.style.display = "none";
-    console.log(button1click);
+    console.log(button2click);
     document.getElementById("game2btn").innerHTML = "Play now!";
+    document.getElementById("game2game").innerHTML = g2Img;
   }
 });
 
@@ -80,11 +84,40 @@ g3Button.addEventListener("click", (e) => {
     g3Img.style.display = "none";
     console.log(button3click);
     document.getElementById("game3btn").innerHTML = "Close game!";
+    document.getElementById("game3game").innerHTML = '<iframe width="560" height="315" allow="fullscreen; autoplay; encrypted-media" src="https://games.construct.net/1/latest" frameborder="0" allowfullscreen="true" msallowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowpaymentrequest="false" referrerpolicy="unsafe-url" sandbox="allow-same-origin allow-forms allow-scripts allow-pointer-lock allow-orientation-lock allow-popups" scrolling="no"></iframe>';
   }
   else if (button3click%2 === 0){
     g3Img.removeAttribute('style');
     g3Game.style.display = "none";
     console.log(button3click);
     document.getElementById("game3btn").innerHTML = "Play now!";
+    document.getElementById("game3game").innerHTML = g3Img;
   }
 });
+
+// game 3
+let g4Reveal = document.querySelector('#game4');
+let g4Game = document.querySelector('#game4game');
+let g4Button = document.querySelector('#game4btn');
+let g4Img = document.querySelector('#game4img')
+
+let button4click = 0;
+
+g4Button.addEventListener("click", (e) => {
+  button4click++;
+  if (button4click%2 === 1){
+    g4Game.removeAttribute('style');
+    g4Img.style.display = "none";
+    console.log(button4click);
+    document.getElementById("game4btn").innerHTML = "Close game!";
+    document.getElementById("game4game").innerHTML = '<iframe width="560" height="315" allow="fullscreen; autoplay; encrypted-media" src="https://games.construct.net/24/latest" frameborder="0" allowfullscreen="true" msallowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" allowpaymentrequest="false" referrerpolicy="unsafe-url" sandbox="allow-same-origin allow-forms allow-scripts allow-pointer-lock allow-orientation-lock allow-popups" scrolling="no"></iframe>';
+  }
+  else if (button4click%2 === 0){
+    g4Img.removeAttribute('style');
+    g4Game.style.display = "none";
+    console.log(button4click);
+    document.getElementById("game4btn").innerHTML = "Play now!";
+    document.getElementById("game4game").innerHTML = g4Img;
+  }
+});
+
